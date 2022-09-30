6c14b808a6c41c4fb8b738a55cea7e34f2d793e6
premnath <premnathkulal1998@gmail.com>
2022-09-30 19:29:41 +0530

    Feature/Initial Screen Implementation is done

---
+++ src/views/LandingView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="landing">
+    <div class="initial-screen">
+      <img src="@/assets/appicon.png" alt="app-icon" />
+      <p class="initial-screen-app-name">MOTORBIKE</p>
+      <p class="initial-screen-app-description">Ride Free</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  components: {},
+})
+export default class HomeView extends Vue {}
+</script>
+
+<style lang="scss">
+.landing {
+  bottom: 0px;
+  width: 100%;
+  min-height: 100vh;
+  background-image: $app-theme-gradient;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  @include flex-align-justify-center();
+
+  .initial-screen {
+    img {
+      width: auto;
+    }
+
+    p {
+      color: $white;
+      font-weight: 600;
+      text-align: center;
+      line-height: 3.5rem;
+    }
+
+    &-app-name {
+      font-size: 2.25rem;
+      font-family: 'Oswald', sans-serif;
+    }
+
+    &-app-description {
+      font-weight: 200;
+      font-size: 1.25rem;
+    }
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+                    @import '~@/scss/_variables.scss';
+                    @import '~@/scss/_reset.scss';
+                    @import '~@/scss/_common.scss';
+                `,
+      },
+    },
+  },
+})
